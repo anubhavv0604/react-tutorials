7ad44182e8b33db32634d760ca977354c5bcf2fd
anubhavv0604 <153982386+anubhavv0604@users.noreply.github.com>
2024-04-15 20:04:23 +0530

    Create README.md

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 const parent = React.createElement("div", {id: "parent"} , [
     React.createElement("div" , {id: "child"} , [
         React.createElement("h1" , {} , "iam an h1 tag"),
