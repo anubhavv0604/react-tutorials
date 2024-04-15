34fe97a5fb3b2baa7c804dcc4aac61760c3678a0
anubhavv0604 <anubhavpra77@gmail.com>
2024-04-15 19:46:11 +0530

    namaste react ep01

---
+++ app.js
@@ -0,0 +1,19 @@
+const parent = React.createElement("div", {id: "parent"} , [
+    React.createElement("div" , {id: "child"} , [
+        React.createElement("h1" , {} , "iam an h1 tag"),
+        React.createElement("h2" , {} , "iam an h2 tag"),
+    ]), 
+    React.createElement("div" , {id: "child2"} , [
+        React.createElement("h1", {} , "i am h1 tag"),
+        React.createElement("h2", {} , "iam an h2 tag"),
+    ]),
+    React.createElement("div" , {id: "child3"} , [
+        React.createElement("h3" , {} , "iam h3 tag"),
+        React.createElement("h4" , {} , "iam an h4 tag"),
+    ]),
+])
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
